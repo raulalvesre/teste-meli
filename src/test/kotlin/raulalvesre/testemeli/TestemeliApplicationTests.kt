2396ed70783592732e7748230ab3f112185e53fd
Raul <raul.alves.re@gmail.com>
2025-11-03 22:14:02 -0300

    inital

---
+++ src/test/kotlin/raulalvesre/testemeli/TestemeliApplicationTests.kt
@@ -0,0 +1,13 @@
+package raulalvesre.testemeli
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestemeliApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
